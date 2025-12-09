f16d15f403ba11658f274969693f26ebc878022a
WordsForNerds <vova.poryadin2215@gmail.com>
2025-12-10 00:01:13 +0500

    Сделал 2 лабу

---
+++ Lab2/Lab2/src/timus/task_1877/task_1877.java
@@ -0,0 +1,50 @@
+package timus.task_1877;
+
+import java.util.Scanner;
+
+public class task_1877 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String stringFirstLock;
+		String stringSecondLock;
+		
+		int firstLock;
+		int secondLock;
+		
+		System.out.println("Введите четырёзначный код от первого замка");
+		stringFirstLock = scan.nextLine();
+		
+		System.out.println("Введите четырёхзначный код от второго замка");
+		stringSecondLock = scan.nextLine();
+		
+        firstLock = Integer.parseInt(stringFirstLock);
+        secondLock = Integer.parseInt(stringSecondLock);
+		
+		for(int i = 0; i < 10000; i++ ) {
+			
+			if (firstLock == i) {
+				
+				System.out.println("yes");
+				System.exit(0);
+				
+			}
+			
+			i++;
+			
+			if (secondLock == i) {
+				
+				System.out.println("yes");
+				System.exit(0);
+				
+			}
+			
+		}
+		
+		System.out.println("no");
+
+	}
+
+}
