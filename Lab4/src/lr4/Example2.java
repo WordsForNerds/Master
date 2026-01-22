dfc23a95ab456d40d5d339382ec1de2df430a371
WordsForNerds <vova.poryadin2215@gmail.com>
2026-01-22 20:21:10 +0500

    Сделал 5 лабу

---
+++ Lab4/src/lr4/Example2.java
@@ -0,0 +1,28 @@
+package lr4;
+
+// 2.Напишите программу, которая выводит в консольное окно прямоугольный треугольник;
+
+public class Example2 {
+    public static void main(String[] args){
+
+        int height = 10; //Число строк, которое необходимо вывести
+        int width = 1;  //Число столбцов, которое необходимо вывести для треугольника
+
+        for (int i = 1 ; i <= height; i++){
+
+            for ( int j = 1; j <= width; j++){
+
+                System.out.print("0");
+
+            }
+            width++ ;
+            System.out.println();
+
+        }
+
+
+
+    }
+
+}
+
---
+++ Lab5/src/Example1/NumberClass.java
@@ -0,0 +1,24 @@
+package Example1;
+
+public class NumberClass {
+
+    private int num;
+
+    public void setValues(int _num){
+
+        num = _num;
+
+    }
+    public Integer getValues(){
+
+        return num;
+
+    }
+
+    public void out(){
+
+        System.out.println("Вот ваше число" + " " + num);
+
+    }
+
+}
